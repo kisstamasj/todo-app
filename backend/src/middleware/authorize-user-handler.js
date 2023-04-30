509445658d2af514ffa59871e323b62a9807befe
Kiss Tamás <kiss.tamas.j@gmail.com>
2023-04-30 19:04:43 +0200

    asd

--- backend/src/middleware/authorize-user-handler.js
+++ backend/src/middleware/authorize-user-handler.js
@@ -1,8 +1,13 @@
 const UnauthorizedError = require('../errors/unauthorized-error');
+const jwt = require('jsonwebtoken');
 
 const authorizeUserHandler = (req, res, next) => {
   const cookiejwt = req.cookies.currentUser;
   if (!cookiejwt) {
+    throw new UnauthorizedError('Unauthorized user!');
+  }
+  const verified = jwt.verify(cookiejwt, process.env.JWT_KEY);
+  if (!verified) {
     throw new UnauthorizedError('Unauthorized user!');
   }
 
