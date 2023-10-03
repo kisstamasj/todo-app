b7fad0c2cf99ffcb81efa3f390495ec37799dcda
Kiss Tamás <kiss.tamas.j@gmail.com>
2023-10-03 23:16:55 +0200

    fix: env

--- client/src/routes/sign-up/sign-up.component.jsx
+++ client/src/routes/sign-up/sign-up.component.jsx
@@ -1,21 +1,21 @@
-import { useContext, useEffect, useState } from 'react';
-import Button from '../../components/button/button.component';
-import FormInput from '../../components/form-input/form-input.component';
-import Form from '../../components/form/form.component';
-import H4 from '../../components/h4/h4.component';
-import axios from 'axios';
-import { UserContext } from '../../contexts/user.context';
-import Alert from '../../components/alert/alert.component';
-import { Link, useNavigate } from 'react-router-dom';
-import { ButtonsContainer } from './sign-up.styles';
-import { FiLogIn } from 'react-icons/fi';
-import { GoAlert } from 'react-icons/go';
-import UnAuthenticated from '../../components/unauthenticated/unauthenticated.component';
+import { useContext, useEffect, useState } from "react";
+import Button from "../../components/button/button.component";
+import FormInput from "../../components/form-input/form-input.component";
+import Form from "../../components/form/form.component";
+import H4 from "../../components/h4/h4.component";
+import axios from "axios";
+import { UserContext } from "../../contexts/user.context";
+import Alert from "../../components/alert/alert.component";
+import { Link, useNavigate } from "react-router-dom";
+import { ButtonsContainer } from "./sign-up.styles";
+import { FiLogIn } from "react-icons/fi";
+import { GoAlert } from "react-icons/go";
+import UnAuthenticated from "../../components/unauthenticated/unauthenticated.component";
 
 const defaultFormFields = {
-  email: '',
-  password: '',
-  confirm_password: '',
+  email: "",
+  password: "",
+  confirm_password: "",
 };
 
 const SignUp = () => {
@@ -25,23 +25,22 @@ const SignUp = () => {
   const { setCurrentUser, currentUser } = useContext(UserContext);
   const handleChange = (event) => {
     const { name, value } = event.target;
-
     setFormfields({ ...formFields, [name]: value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setErrors('');
+    setErrors("");
     if (formFields.password !== formFields.confirm_password) {
-      return setErrors('The password and Confirm password dont match!');
+      return setErrors("The password and Confirm password dont match!");
     }
 
     try {
-      const { data } = await axios.post('/api/users/signup', formFields);
+      const { data } = await axios.post("/api/users/signup", formFields);
       setCurrentUser(data);
-      setErrors('');
+      setErrors("");
       setFormfields(defaultFormFields);
-      navigate('/todos');
+      navigate("/todos");
     } catch (error) {
       if (error.response.data.message) {
         setErrors(error.response.data.message);
@@ -51,7 +50,7 @@ const SignUp = () => {
 
   useEffect(() => {
     if (currentUser) {
-      navigate('/todos');
+      navigate("/todos");
     }
   }, [currentUser, navigate]);
 
@@ -62,36 +61,36 @@ const SignUp = () => {
       <Form onSubmit={handleSubmit}>
         <FormInput
           required
-          type='email'
-          name='email'
-          label={'Email'}
+          type="email"
+          name="email"
+          label={"Email"}
           onChange={handleChange}
           value={formFields.email}
         />
         <FormInput
           required
-          type='password'
-          name='password'
-          label={'Password'}
+          type="password"
+          name="password"
+          label={"Password"}
           onChange={handleChange}
           value={formFields.password}
         />
         <FormInput
           required
-          type='password'
-          name='confirm_password'
-          label={'Confirm password'}
+          type="password"
+          name="confirm_password"
+          label={"Confirm password"}
           onChange={handleChange}
           value={formFields.confirm_password}
         />
         {errors && (
-          <Alert type='danger'>
+          <Alert type="danger">
             <GoAlert /> {errors}
           </Alert>
         )}
         <ButtonsContainer>
-          <Link to={'/'}>Back to login</Link>
-          <Button buttonType={'primary'} type='submit'>
+          <Link to={"/"}>Back to login</Link>
+          <Button buttonType={"primary"} type="submit">
             <FiLogIn /> Sign up
           </Button>
         </ButtonsContainer>
